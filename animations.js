04bbb02746ab0c590bcf20df66adc50b70794a4d
AnderMartin330 <65553583+AnderMartin330@users.noreply.github.com>
2024-05-19 22:35:16 +0200

    Add files via upload

---
+++ animations.js
@@ -0,0 +1,18 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+/*function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "#401F71"; //rgba(0,0,0,0.4)
+  x.classList.toggle("change");
+}*/
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+/*function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.body.style.backgroundColor = "#401F71";
+  x.classList.toggle("change");
+}
+/*function myFunction(x) {
+   
+  }*/
